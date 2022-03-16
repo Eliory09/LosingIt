aaea501814bf07be145fc592b31aa28f95a9423f
Elior Digmi <Eliory09@gmail.com>
2022-03-16 12:38:45 +0200

    Added Ball Movement Mechanic

---
+++ Assets/Mechanics/Scripts/M_BallMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_BallMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D physics;
+    [SerializeField] private float speed;
+    private bool _areArrowsPressed;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            physics.AddForce(Vector2.up * speed);
+            _areArrowsPressed = true;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            physics.AddForce(Vector2.down * speed);
+            _areArrowsPressed = true;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            physics.AddForce(Vector2.left * speed);
+            _areArrowsPressed = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            physics.AddForce(Vector2.right * speed);
+            _areArrowsPressed = true;
+        }
+        
+        if (_areArrowsPressed)
+        {
+            _areArrowsPressed = false;
+        }
+        else
+        {
+            physics.AddForce(-physics.velocity.normalized * speed);
+        }
+    }
+}
