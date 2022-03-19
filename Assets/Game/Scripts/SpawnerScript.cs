cd4a7726488e457adfefc2c3680c4862e18f6247
Elior Digmi <Eliory09@gmail.com>
2022-03-19 14:16:17 +0200

    Minor changes

--- Assets/Game/Scripts/SpawnerScript.cs
+++ Assets/Game/Scripts/SpawnerScript.cs
@@ -19,14 +19,33 @@ public class SpawnerScript : MonoBehaviour
         {
             var pos = Camera.main.transform.position;
             var roundX = Mathf.RoundToInt(pos.x);
-            var roundY = Mathf.RoundToInt(pos.y) + 6f;
+            var roundY = Mathf.RoundToInt(pos.y) + 6;
             var newPos =  new Vector3(roundX, roundY, 10);
             transform.position = newPos;
         }
     }
 
     public void NewTetrisBlock()
-    { 
-        Instantiate(tetrisBlocks[Random.Range(0, tetrisBlocks.Length)], transform.position, Quaternion.identity);
+    {
+        if (IsValidToSpawn())
+        {
+            Instantiate(tetrisBlocks[Random.Range(0, tetrisBlocks.Length)], transform.position, Quaternion.identity);    
+        }
+    }
+
+    private bool IsValidToSpawn()
+    {
+        var position = transform.position;
+        var xPos = Mathf.RoundToInt(position.x);
+        var yPos = Mathf.RoundToInt(position.y);
+        for (int i = xPos - 9; i <= xPos + 9; i++)
+        {
+            if (TetrisBlock.grid[xPos, yPos])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
