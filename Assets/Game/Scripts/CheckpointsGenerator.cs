484605a618f6fd84ef46568cb4ef2df61f7c088c
Elior Digmi <Eliory09@gmail.com>
2022-03-25 00:18:45 +0200

    Checkpoints fix

--- Assets/Game/Scripts/CheckpointsGenerator.cs
+++ Assets/Game/Scripts/CheckpointsGenerator.cs
@@ -17,6 +17,7 @@ public class CheckpointsGenerator : MonoBehaviour
     [SerializeField] private GameObject checkpointObj;
     [SerializeField] private GameObject ball;
     private GameObject currentCheckpoint;
+    private int direction;
     
     private Vector3 _currentPoint = Vector3.zero;
     private static CheckpointsGenerator _shared;
@@ -24,13 +25,25 @@ public class CheckpointsGenerator : MonoBehaviour
     private void Awake()
     {
         _shared = this;
+        _shared.direction = 1;
     }
 
     public static void GenerateNewPoint()
     {
-        
         var delta = 90;
-        var theta = Mathf.Deg2Rad * (Random.Range(-_shared.angleDelta, _shared.angleDelta) + delta);
+        float minDelta, maxDelta;
+        
+        if (_shared.direction == 1)
+        {
+            minDelta = 0;
+            maxDelta = _shared.angleDelta;
+        }
+        else
+        {
+            minDelta = -_shared.angleDelta;
+            maxDelta = 0;
+        }
+        var theta = Mathf.Deg2Rad * (Random.Range(minDelta, maxDelta) + delta);
         _shared._currentPoint = 
             new Vector3(
                 (float) (_shared.radius * Math.Cos(theta)), 
@@ -42,6 +55,7 @@ public class CheckpointsGenerator : MonoBehaviour
         _shared.currentCheckpoint = Instantiate(_shared.checkpointObj);
         _shared.currentCheckpoint.transform.position = _shared._currentPoint;
         _shared.currentCheckpoint.GetComponent<Checkpoint>().AddCheckpointToGrid();
+        _shared.direction *= -1;
         CinemachineCamerasController.AddCamera(_shared.currentCheckpoint.transform.position);
     }
 
